87dfa458811f10600ea3bf1e8b80e6be78f97860
Ashok verma <ashokverma3779@gmail.com>
2020-06-09 00:17:35 +0530

    Introduction to php Hello World

---
+++ introduction/program1.php
@@ -0,0 +1,32 @@
+<?php
+
+# Powerful , Free Open Source Programming language
+# Widely used in web Development
+# Fast, Stable , Cross Platform
+
+
+# php -> PHP originally stood for Personal Home Page, but it now stands for the recursive initialism PHP: Hypertext Preprocessor.
+
+# Runs on Server Linux, Windows , Mac
+
+# Testing Server
+
+# LAMP -> Linux
+# WAMP -> Windows
+# MAMP -> Mac
+# XAMPP-> Universal (Linux, Windows , Mac)
+
+# AMP Means
+# A-> Apache
+# M-> Mysql or Mysqli
+# P-> PHP
+
+# php process script on a web Server
+# php is a server side language
+# Javascript , HTML , CSS are client side languages
+
+# Run php file on server start 'localhost'
+
+echo 'Hello World';
+
+?>
